07c55c4320e18c6eb414565eacaddc045c4f325b
Christopher Chang <chris.chang1989@gmail.com>
2023-09-22 12:21:00 -0700

    2023-09-22 Friday Daily Codewars - Enumerable Magic - Does My List Include This?

---
+++ 2023-09-22 Codewars 8 Kyu Fundamentals - Enumerable Magic - Does My List Include This.js
@@ -0,0 +1,21 @@
+// 9/22/23 Friday Codewars 8 Kyu Fundamentals - Enumerable Magic - Does My List Include This?
+
+// https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item){
+    return arr.includes(item);
+}
+
+// As arrow function
+const include = (arr, item) => arr.includes(item);
+
+
+
+
+/* =============
+Other Solutions
+============= */
