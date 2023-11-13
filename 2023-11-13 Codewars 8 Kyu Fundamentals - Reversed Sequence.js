825de0d46fc5e34a66f23fa4eebfe96df2ca7879
Christopher Chang <chris.chang1989@gmail.com>
2023-11-13 14:11:27 -0800

    Added more efficient solution from other solutions

--- 2023-11-13 Codewars 8 Kyu Fundamentals - Reversed Sequence.js
+++ 2023-11-13 Codewars 8 Kyu Fundamentals - Reversed Sequence.js
@@ -39,3 +39,12 @@ const reverseSeq = n => {
 /* =============
 Other Solutions
 ============= */
+const reverseSeq = n => {
+    return Array(n).fill(0).map((e, i) => n - i );
+};
+/* Explanation:
+
+    1. Array(n) creates an array with n number of empty spots. Ex: Array(5) gives you this: [ undefined, undefined, undefined, undefined, undefined ].
+    2. Then comes fill(0), the fill() method changes all elements in an array to a static value(MDN Web docs). Ex: Our array from the example above, after applying .fill(0) would be [0, 0, 0, 0, 0].
+    3. Finally .map takes each element of the array and runs a provided function on each element. Ex: [0, 0, 0, 0, 0].map( ( e, i ) => n-i ); equals to [5,4,3,2,1].
+*/
