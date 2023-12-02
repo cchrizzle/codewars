41b7d5485f6305dccee7a039452a298ee7e829e7
Christopher Chang <chris.chang1989@gmail.com>
2023-12-02 13:58:09 -0800

    2023-12-02 Saturday Daily Codewars - Third Angles of a Triangle

---
+++ 2023-12-02 Codewars 8 Kyu Fundamentals - Third Angle of a Triangle.js
@@ -0,0 +1,25 @@
+// 12/2/23 Saturday Codewars 8 Kyu Fundamentals - Third Angle of a Triangle
+
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+/*
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+
+*/
+
+function otherAngle(a, b) {
+    return 180 - (a + b);
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
