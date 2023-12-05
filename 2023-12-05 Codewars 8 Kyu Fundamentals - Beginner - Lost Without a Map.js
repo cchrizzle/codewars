5d51d96c4c2964b4cd85966d08ce743b3ec70a88
Christopher Chang <chris.chang1989@gmail.com>
2023-12-05 02:08:54 -0800

    2023-12-05 Tuesday Daily Codewars - Beginner - Lost Without a Map

---
+++ 2023-12-05 Codewars 8 Kyu Fundamentals - Beginner - Lost Without a Map.js
@@ -0,0 +1,23 @@
+// 12/5/23 Tuesday Codewars 8 Kyu Fundamentals - Beginner - Lost Without a Map
+
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+function maps(x){
+    return x.map(n => n * 2);
+}
+
+// As arrow function
+const maps = x => x.map(n => n * 2);
+
+
+/* =============
+Other Solutions
+============= */
