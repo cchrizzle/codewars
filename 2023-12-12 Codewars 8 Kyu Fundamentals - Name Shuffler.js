083233a4c1dad17dfb33183feade1fcaad901bfa
Christopher Chang <chris.chang1989@gmail.com>
2023-12-12 11:41:25 -0800

    2023-12-12 Tuesday Daily Codewars - Name Shuffler

---
+++ 2023-12-12 Codewars 8 Kyu Fundamentals - Name Shuffler.js
@@ -0,0 +1,22 @@
+// 12/12/23 Tuesday Codewars 8 Kyu Fundamentals - Name Shuffler
+
+// https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
+
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+*/
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ');
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
