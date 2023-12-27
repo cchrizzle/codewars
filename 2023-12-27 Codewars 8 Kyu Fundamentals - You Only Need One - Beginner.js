e4e578fe61092ff8b6c217a5021e5531a84f6535
Christopher Chang <chris.chang1989@gmail.com>
2023-12-27 15:33:50 -0800

    2023-12-27 Wednesday Daily Codewars - You Only Need One - Beginner

---
+++ 2023-12-27 Codewars 8 Kyu Fundamentals - You Only Need One - Beginner.js
@@ -0,0 +1,22 @@
+// 12/27/23 Wednesday Codewars 8 Kyu Fundamentals - You Only Need One - Beginner
+
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+function check(a, x) {
+    return a.includes(x);
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
