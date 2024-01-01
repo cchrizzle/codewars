c160c2c6849bae0f28f66539b671bd12087702f4
Christopher Chang <chris.chang1989@gmail.com>
2024-01-01 14:08:52 -0800

    Added some other solutions

--- 2024-01-01 Codewars 7 Kyu Fundamentals - Mumbling.js
+++ 2024-01-01 Codewars 7 Kyu Fundamentals - Mumbling.js
@@ -60,3 +60,29 @@ function accum(s) {
 /* =============
 Other Solutions
 ============= */
+function accum(s) {
+	return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
+
+
+
+
+// 2nd solution
+function accum(str) {
+	var letters = str.split('');
+  var result = [];
+  for (var i = 0; i < letters.length; i++) {
+    result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+  return result.join('-');
+}
+    /* The Array function is used to create a new array. It is capitalized because it is the standard for constructor functions to be capitalized.
+
+    Array(2) creates an array of 2 blank values.
+
+    In this case it is being used to create an empty array and join the array together using whichever character is being iterated over in the loop. Since there are no contents in the array, using this with the join() function works like a multiplier for the character being passed into join. For example Array(3).join("x") would evaluate to xx. You need one more item in the array since join() adds characters between array objects, so three empty arrays would be seperated by 2 join characters.
+    */
+
+    /*
+    Basically a new Array is being defined with a given length, and it's an empty array. Then this empty array is being joined, with a given letter joining the empty indexes. For example, for i = 2, the Array has 3 empty indexes: [,,] and then it is joined by whatever letter is in letters[i]. Say letters[2] equals the letter a. Then if we join the empty array together with the letter in between each empty index, you get "aa".
+    */
