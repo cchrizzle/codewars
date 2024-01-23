97e4164977efc52a84827dda8947d86b402b0209
Christopher Chang <chris.chang1989@gmail.com>
2024-01-23 12:25:59 -0800

    2024-01-23 Tuesday Daily Codewars - Find the Position

---
+++ 2024-01-23 Codewars 8 Kyu Fundamentals - Find the Position.js
@@ -0,0 +1,31 @@
+// UNDATED Day Codewars 8 Kyu Fundamentals - Find the Position!
+
+// https://www.codewars.com/kata/5808e2006b65bff35500008f/train/javascript
+
+/*
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+*/
+
+function position(letter){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    let alphabetArray = alphabet.split('');
+
+    return `Position of alphabet: ${alphabetArray.indexOf(letter) + 1}`;
+}
+
+
+
+/* =============
+Other Solutions
+============= */
+// Didn't need to split alphabet
+function position(letter){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+}
