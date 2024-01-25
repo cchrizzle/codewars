0c05ced59fecafa1f95f73eecf7d99c5c4969702
Christopher Chang <chris.chang1989@gmail.com>
2024-01-25 15:10:38 -0800

    2024-01-25 Thursday Daily Codewars - Duplicate Encoder

---
+++ 2024-01-25 Codewars 6 Kyu Fundamentals - Duplicate Encoder.js
@@ -0,0 +1,49 @@
+// 1/25/24 Thursday Codewars 6 Kyu Fundamentals - Duplicate Encoder
+
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+Examples
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+
+Notes
+
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+*/
+
+function duplicateEncode(word){
+    let letterCount = {};
+    let splitWord = word.toLowerCase().split('');
+
+    splitWord.forEach(letter => {
+        if(letter in letterCount) {
+            letterCount[letter] += 1;
+        } else {
+            letterCount[letter] = 1;
+        }
+    })
+
+    let newString = '';
+    splitWord.forEach(letter => {
+        if(letterCount[letter] === 1) {
+            newString += '(';
+        } else {
+            newString += ')'
+        }
+    })
+
+    return newString;
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
