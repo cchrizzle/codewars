b1592f6f72728cb76bfb86d58bd1f8662cd5e2c0
Christopher Chang <chris.chang1989@gmail.com>
2024-01-31 10:15:07 -0800

    2024-01-31 Wednesday Daily Codewars - Regular Ball Super Ball

---
+++ 2024-01-31 Codewars 8 Kyu Fundamentals - Regular Ball Super Ball.js
@@ -0,0 +1,26 @@
+// 1/31/24 Wednesday Codewars 8 Kyu Fundamentals - Regular Ball Super Ball
+
+// https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+var Ball = function(ballType) {
+    this.ballType = ballType || 'regular';
+};
+
+
+
+
+/* =============
+Other Solutions
+============= */
