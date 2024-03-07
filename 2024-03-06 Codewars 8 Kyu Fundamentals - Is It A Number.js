ea4b7fd73b9e157f4707bac8a6d8b7e8e712145c
Christopher Chang <chris.chang1989@gmail.com>
2024-03-06 17:44:20 -0800

    2024-03-06 Wednesday Daily Codewars - Is It a Number?

---
+++ 2024-03-06 Codewars 8 Kyu Fundamentals - Is It A Number.js
@@ -0,0 +1,34 @@
+// 3/6/24 Wednesday Codewars 8 Kyu Fundamentals - Is It A Number?
+
+// https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/javascript
+
+/*
+Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+
+should return false:
+
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+*/
+
+function isDigit(s) {
+    return s==parseFloat(s);
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
+function isDigit(s) {
+    return parseFloat(s) === Number(s)
+}
