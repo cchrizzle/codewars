ceb5897bbe89be4f11d44ebdc294ac9516a5ad43
Christopher Chang <chris.chang1989@gmail.com>
2024-03-10 10:17:48 -0700

    2024-03-10 Sunday Daily Codewars - Type of Sum

---
+++ 2024-03-10 Codewars 8 Kyu Fundamentals - Type of Sum.js
@@ -0,0 +1,12 @@
+// 3/10/24 Sunday Codewars 8 Kyu Fundamentals - Type of Sum
+
+// https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/train/javascript
+
+/*
+Return the type of the sum of the two arguments
+*/
+
+function typeOfSum(a, b) {
+    let sum = a + b;
+    return typeof(sum);
+}
