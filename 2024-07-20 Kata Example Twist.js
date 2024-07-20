af155adb10c3eb6f8edc4126a3c4a49ce31f5356
Christopher Chang <chris.chang1989@gmail.com>
2024-07-20 12:22:20 -0700

    2024-07-20 Saturday Daily Codewars - Kata Example Twist

---
+++ 2024-07-20 Kata Example Twist.js
@@ -0,0 +1,18 @@
+// 7/20/24 Saturday Codewars 8 Kyu Fundamentals - Kata Example Twist
+
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+
+/*
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+*/
+
+// Code
+
+
+
+
+/* =============
+Other Solutions
+============= */
