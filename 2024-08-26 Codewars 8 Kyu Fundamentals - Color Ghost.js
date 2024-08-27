456439f21c1bb45b15677312f01d4ea4cd0aee92
Christopher Chang <chris.chang1989@gmail.com>
2024-08-26 22:26:49 -0700

    2024-08-26 Monday Daily Codewars - Color Ghost

---
+++ 2024-08-26 Codewars 8 Kyu Fundamentals - Color Ghost.js
@@ -0,0 +1,30 @@
+// 8/26/24 Monday Codewars 8 Kyu Fundamentals - Color Ghost
+
+// https://www.codewars.com/kata/53f1015fa9fe02cbda00111a/train/javascript
+
+/*
+Color Ghost
+
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"
+*/
+
+class Ghost {
+    constructor() {
+        const colors = ['white', 'yellow', 'purple', 'red'];
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+    }
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
