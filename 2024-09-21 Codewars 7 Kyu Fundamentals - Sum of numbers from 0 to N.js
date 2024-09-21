a2566b3624b41ac68ccb97e866804bf8a29b3784
Christopher Chang <chris.chang1989@gmail.com>
2024-09-21 13:39:35 -0700

    2024-09-21 Saturday Daily Codewars - Sum of Numbers From 0 to N

---
+++ 2024-09-21 Codewars 7 Kyu Fundamentals - Sum of numbers from 0 to N.js
@@ -0,0 +1,53 @@
+// 9/21/24 Saturday Codewars 7 Kyu Fundamentals - Sum of numbers from 0 to N
+
+// https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763/train/javascript
+
+/*
+Description:
+
+We want to generate a function that computes the series starting from 0 and ending until the given number.
+Example:
+
+Input:
+
+> 6
+
+Output:
+
+    0+1+2+3+4+5+6 = 21
+
+Input:
+
+> -15
+
+Output:
+
+    -15<0
+
+Input:
+
+> 0
+
+Output:
+
+    0=0
+*/
+
+var SequenceSum = (function() {
+    function SequenceSum() {}
+  
+    SequenceSum.showSequence = function(count) {
+      // for
+        //while
+    };
+  
+    return SequenceSum;
+  
+  })();
+
+
+
+
+/* =============
+Other Solutions
+============= */
