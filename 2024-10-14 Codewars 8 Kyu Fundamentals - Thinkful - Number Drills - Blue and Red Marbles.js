abe2abf02a8667325a58b8833a0c7ed062e23210
Christopher Chang <chris.chang1989@gmail.com>
2024-10-14 12:26:55 -0700

    2024-10-14 Monday Daily Codewars - Thinkful - Number Drills - Blue and Red Marbles

---
+++ 2024-10-14 Codewars 8 Kyu Fundamentals - Thinkful - Number Drills - Blue and Red Marbles.js
@@ -0,0 +1,34 @@
+// 10/14/24 Monday Codewars 8 Kyu Fundamentals - Thinkful - Number Drills - Blue and Red Marbles
+
+// https://www.codewars.com/kata/5862f663b4e9d6f12b00003b/train/javascript
+
+/*
+You and a friend have decided to play a game to drill your statistical intuitions. The game works like this:
+
+You have a bunch of red and blue marbles. To start the game you grab a handful of marbles of each color and put them into the bag, keeping track of how many of each color go in. You take turns reaching into the bag, guessing a color, and then pulling one marble out. You get a point if you guessed correctly. The trick is you only have three seconds to make your guess, so you have to think quickly.
+
+You've decided to write a function, guessBlue() to help automatically calculate whether you should guess "blue" or "red". The function should take four arguments:
+
+    the number of blue marbles you put in the bag to start
+    the number of red marbles you put in the bag to start
+    the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+    the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+
+guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+
+*/
+
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    const blueRemaining = blueStart - bluePulled;
+    const redRemaining = redStart - redPulled;
+    const total = blueRemaining + redRemaining;
+
+    return parseFloat(blueRemaining / total);
+}
+
+
+
+
+/* =============
+Other Solutions
+============= */
