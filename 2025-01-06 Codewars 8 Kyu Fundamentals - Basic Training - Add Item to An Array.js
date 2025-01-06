f667dcb0eed9e3f54f8faafccd4275d594307904
Christopher Chang <chris.chang1989@gmail.com>
2025-01-06 10:15:42 -0800

    Daily Codewars - Basic Training - Add Item to An Array

---
+++ 2025-01-06 Codewars 8 Kyu Fundamentals - Basic Training - Add Item to An Array.js
@@ -0,0 +1,14 @@
+// 1/6/25 Monday Codewars 8 Kyu Fundamentals - Basic Training: Add Item to An Array
+
+// https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+websites.push('codewars');
