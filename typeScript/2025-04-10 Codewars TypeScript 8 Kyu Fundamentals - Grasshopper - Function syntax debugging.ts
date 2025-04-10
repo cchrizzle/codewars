9a81a791b558da97017bbdb827ce04250a938b3b
Christopher Chang <chris.chang.dev@proton.me>
2025-04-10 14:02:41 -0700

    Grasshopper - Function Syntax Debugging

---
+++ typeScript/2025-04-10 Codewars TypeScript 8 Kyu Fundamentals - Grasshopper - Function syntax debugging.ts
@@ -0,0 +1,21 @@
+// 4/10/25 Thursday Codewars TypeScript 8 Kyu Fundamentals - Grasshopper - Function syntax debugging
+
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/typescript
+
+/*
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+Given code:
+export function main [verb: string, noun: string]: string
+  return verb + noun
+}
+*/
+
+// Code
+export function main(verb: string, noun: string): string {
+  return verb + noun;
+}
+
+/* =============
+Other Solutions
+============= */
