c040c54f774fe485526847d6d506707a58cf2991
Christopher Chang <chris.chang.dev@proton.me>
2025-04-14 13:26:42 -0700

    Codewars TypeScript - Is Integer Safe to Use?

---
+++ typeScript/2025-04-14 Codewars TypeScript 8 Kyu Fundamentals - Is Integer Safe to Use.ts
@@ -0,0 +1,22 @@
+// 4/14/25 Monday Codewars TypeScript 8 Kyu Fundamentals - Is Integer Safe to Use?
+
+// https://www.codewars.com/kata/55a4f9afeffe4231090000d6/train/typescript
+
+/*
+Not all integers can be represented by JavaScript/TypeScript. It has space to to represent 53bit signed integers. In this Kata, we've to determine if it is safe to use the integer or not. Make use of the latest ES6 features to find this.
+
+SafeInteger(9007199254740990) //true
+SafeInteger(-90) //true
+SafeInteger(9007199254740992) //false
+*/
+
+export function SafeInteger(n: number): boolean {
+    const min = Number.MIN_SAFE_INTEGER;
+    const max = Number.MAX_SAFE_INTEGER;
+    return n >= min && n <= max;
+}
+
+/* =============
+Other Solutions
+============= */
+export const SafeInteger = Number.isSafeInteger;
