41ef4c5d0a8bc050ae4058d61f665a7357f2bd49
Christopher Chang <chris.chang.dev@proton.me>
2025-04-23 11:47:11 -0700

    TypeScript Daily Codewars - Maximum Triplet Sum (Array Series #7)

---
+++ typeScript/2025-04-23 Codewars TS 8 Kyu Fundamentals - Maximum Triplet Sum (Array Series #7).ts
@@ -0,0 +1,54 @@
+// 4/23/25 Wednesday Codewars TypeScript 8 Kyu Fundamentals - Maximum Triplet Sum (Array Series #7) 
+
+// https://www.codewars.com/kata/5aa1bcda373c2eb596000112/train/typescript
+
+/*
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+Notes :
+
+    Array/list size is at least 3 .
+
+    Array/list numbers could be a mixture of positives , negatives and zeros .
+
+    Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+    Input >> Output Examples
+
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+
+Explanation:
+
+    As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+    Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+    2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+
+    Explanation:
+
+    As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+
+    Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+    3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+
+    Explanation:
+
+    As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+
+    Note : duplications are not included when summing , (i.e) the numbers added only once .
+    
+    Playing with Numbers Series (https://www.codewars.com/collections/playing-with-numbers)
+    Playing With Lists/Arrays Series (https://www.codewars.com/collections/playing-with-lists-slash-arrays)
+    For More Enjoyable Katas (http://www.codewars.com/users/MrZizoScream/authored)
+*/
+
+export function maxTriSum(nums: number[]): number {
+    const sortNums = nums.sort((a, b) => b - a);
+    const sortedSet = new Set(sortNums);
+
+
+    return sortedSet[0] + sortedSet[1] + sortedSet[2];
+}
+/* =============
+Other Solutions
+============= */
